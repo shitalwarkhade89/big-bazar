0453a0c08f909c497d5a509b547720c23b768790
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-10-26 15:43:24 +0530

    mongodb connected successfully

--- server/index.js
+++ server/index.js
@@ -7,8 +7,18 @@ dotenv.config();
 const app =express();
 app.use(express.json());
 
+const connectDB =async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    if (conn){
+        console.log(`MongoDb Connected Succesfully`);
+    }
+};
+
+
 const PORT  = process.env.PORT ||5000;
 
 app.listen(PORT,() => {
     console.log (`server running on port :${PORT}`)
+    connectDB();
+
 });
