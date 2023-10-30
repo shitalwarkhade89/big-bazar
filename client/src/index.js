3dcc895731e1393dc400b7b53b26c571f2822e25
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-10-30 23:25:59 +0530

    creare root for login and singup page

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom/client';
+import{createBrowserRouter ,RouterProvider} from 'react-router-dom';
+import Login from './views/Login/Login';
+import Singup from './views/Singup/Singup';
+const root = ReactDom.createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const router= createBrowserRouter([
+    {
+        path:'/',
+        element:<h1>Home</h1>
+    },
+    {
+        path:'/login',
+        element:<Login/>
+    },
+    {
+        path:'/singup',
+        element:<Singup/>
+    },
+
+])
+
+root.render(<RouterProvider router={router}/>);
