41bc4f22e3822e7570665e20c366b73c76ea2747
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2023-11-18 23:25:35 -0800

    create responsive navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ function Navbar() {
         <>
             <div className="navbar">
                 <Link to="/" className="navbar-logo">Big-Bazar 🛒</Link>
-                <div>
+                <div className="navbar-links-cont">
                     <Link to="/login" className="navbar-links">Login</Link>
                     <Link to="/signup" className="navbar-links">Signup</Link>
                     <Link to="orders" className="navbar-links">My Orders</Link>
